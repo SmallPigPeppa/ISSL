d21a7f74270462bde7012913b97edceb7a515ba4
lwz <905328134@qq.com>
2023-04-14 22:33:14 +0800

    initial

---
+++ bash_files/continual/cifar/barlow_reparam_3x3_data.sh
@@ -0,0 +1,36 @@
+python3 main_continual_reparam.py \
+    --dataset cifar100 \
+    --encoder resnet18_cifar_reparam_3x3 \
+    --data_dir $DATA_DIR \
+    --split_strategy data \
+    --task_idx 0 \
+    --max_epochs 500 \
+    --num_tasks 5 \
+    --gpus 0 \
+    --num_workers 4 \
+    --precision 16 \
+    --optimizer sgd \
+    --lars \
+    --grad_clip_lars \
+    --eta_lars 0.02 \
+    --exclude_bias_n_norm \
+    --scheduler warmup_cosine \
+    --lr 0.3 \
+    --classifier_lr 0.1 \
+    --weight_decay 1e-4 \
+    --batch_size 256 \
+    --brightness 0.4 \
+    --contrast 0.4 \
+    --saturation 0.2 \
+    --hue 0.1 \
+    --gaussian_prob 0.0 0.0 \
+    --solarization_prob 0.0 0.2 \
+    --name reparam-3x3-data \
+    --project ISSL-barlow \
+    --entity pigpeppa \
+    --wandb \
+    --save_checkpoint \
+    --method barlow_twins \
+    --proj_hidden_dim 2048 \
+    --output_dim 2048 \
+    --scale_loss 0.1
