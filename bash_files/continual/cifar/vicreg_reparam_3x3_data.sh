b3e828f75be14e4fc3272b9910751c5a43888c39
lwz <905328134@qq.com>
2023-04-15 19:49:00 +0800

    initial

---
+++ bash_files/continual/cifar/vicreg_reparam_3x3_data.sh
@@ -0,0 +1,38 @@
+python3 main_continual_reparam.py \
+    --dataset cifar100 \
+    --encoder resnet18_cifar_reparam_3x3 \
+    --data_dir $DATA_DIR \
+    --split_strategy data \
+    --task_idx 0 \
+    --max_epochs 500 \
+    --num_tasks 5 \
+    --gpus 0 \
+    --precision 16 \
+    --optimizer sgd \
+    --lars \
+    --grad_clip_lars \
+    --eta_lars 0.02 \
+    --exclude_bias_n_norm \
+    --scheduler warmup_cosine \
+    --lr 0.3 \
+    --weight_decay 1e-4 \
+    --batch_size 256 \
+    --num_workers 3 \
+    --min_scale 0.2 \
+    --brightness 0.4 \
+    --contrast 0.4 \
+    --saturation 0.2 \
+    --hue 0.1 \
+    --solarization_prob 0.1 \
+    --gaussian_prob 0.0 0.0 \
+    --name reparam-3x3-data \
+    --project ISSL-vicreg \
+    --entity pigpeppa \
+    --wandb \
+    --save_checkpoint \
+    --method vicreg \
+    --proj_hidden_dim 2048 \
+    --output_dim 2048 \
+    --sim_loss_weight 25.0 \
+    --var_loss_weight 25.0 \
+    --cov_loss_weight 1.0 
