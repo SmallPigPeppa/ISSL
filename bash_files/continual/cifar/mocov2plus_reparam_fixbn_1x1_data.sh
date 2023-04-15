86fc146acf4e900e163b24d618c41e35bf2c18c4
lwz <905328134@qq.com>
2023-04-15 20:49:34 +0800

    initial

---
+++ bash_files/continual/cifar/mocov2plus_reparam_fixbn_1x1_data.sh
@@ -0,0 +1,36 @@
+python3 main_continual_reparam_fixbn.py \
+    --dataset cifar100 \
+    --encoder resnet18_cifar_reparam \
+    --data_dir $DATA_DIR \
+    --split_strategy data \
+    --task_idx 0 \
+    --max_epochs 500 \
+    --num_tasks 5 \
+    --gpus 0 \
+    --precision 16 \
+    --optimizer sgd \
+    --lars \
+    --grad_clip_lars \
+    --eta_lars 0.02 \
+    --scheduler warmup_cosine \
+    --lr 0.3 \
+    --classifier_lr 0.3 \
+    --weight_decay 1e-4 \
+    --batch_size 256 \
+    --num_workers 4 \
+    --brightness 0.4 \
+    --contrast 0.4 \
+    --saturation 0.4 \
+    --hue 0.1 \
+    --gaussian_prob 0.0 0.0 \
+    --name reparam-fixbn-1x1-data \
+    --project ISSL-mocov2plus \
+    --entity pigpeppa \
+    --wandb \
+    --save_checkpoint \
+    --method mocov2plus \
+    --proj_hidden_dim 2048 \
+    --queue_size 65536 \
+    --temperature 0.2 \
+    --base_tau_momentum 0.99 \
+    --final_tau_momentum 0.999
